072f8d0dc823f0d1f22aa577732ffe850e322621
Harshit Maurya <hmaurya999@gmail.com>
2019-11-27 22:32:30 +0530

    add home route to brand logo

--- frontend/src/components/Header/HeaderBranding.js
+++ frontend/src/components/Header/HeaderBranding.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import logo from '../../assets/images/logo.svg'
+import {Link} from 'react-router-dom';
 
 export default class HeaderBranding extends Component {
     render() {
         return (
-            <div className="logo-container">
-                <img className="smp-logo" src={logo} alt='smp-logo'></img>
-                <div className="logo-name-container">
-                    <span className="color-red">SMP</span>
-                    <span className="color-grey">-</span>
-                    <span className="color-blue">IITR</span>
-                </div>
+            <div>
+                <Link to="/">
+                    <div className="logo-container">
+                        <img className="smp-logo" src={logo} alt='smp-logo'></img>
+                        <div className="logo-name-container">
+                            <span className="color-red">SMP</span>
+                            <span className="color-grey">-</span>
+                            <span className="color-blue">IITR</span>
+                        </div>
+                    </div>
+                </Link>
             </div>
         )
     }
