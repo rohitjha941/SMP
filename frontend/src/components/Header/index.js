00d7be677a865a1a99cdb3a6580e86525fbfeb4b
Harshit Maurya <hmaurya999@gmail.com>
2019-12-02 00:20:42 +0530

    add hamburger to header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import HeaderBranding from './HeaderBranding';
 import HeaderRoutes from './HeaderRoutes';
+import Hamburger from './Hamburger';
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {showMenu: false}
+    }
+
+    toggleMenu() {
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+    }
+
+    hideMenu() {
+        this.setState({
+            showMenu: false
+        })
+    }
     render() {
         return (
-            <div>
+            <div className="header-container">
                 <header>
                     <div className="header-items">
-                        <HeaderBranding />
-                        <HeaderRoutes />
+                        <HeaderBranding hideMenu={() => {this.hideMenu()}} />
+                        <Hamburger className="hamburger" showMenu={this.state.showMenu} toggleMenu={() => {
+                            this.toggleMenu()
+                        }} />
+                        <HeaderRoutes showMenu={this.state.showMenu} hideMenu={() => {this.hideMenu()}} />
                     </div>
                 </header>
             </div>
