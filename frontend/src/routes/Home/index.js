5899ae65df2ab2ed14afec65f3308c3bed60b97b
Harshit Maurya <hmaurya999@gmail.com>
2019-12-12 10:02:36 +0530

    change home style

--- frontend/src/routes/Home/index.js
+++ frontend/src/routes/Home/index.js
@@ -4,13 +4,14 @@ import styles from './Home.module.scss';
 import people_plant_light from 'assets/images/people_plant_light.svg';
 import Title from 'components/Title';
 import TextView from 'components/TextView';
+import Button from 'components//Button';
 
 export default class Home extends Component {
     render() {
         const title = "Fostering Senior Junior Relationship"
         const description = "Student Mentorship Program or the SMP assigns one mentor for a group of 5-9 facchas, a third or fourth yearite from their respective branches who can guide the incoming freshmen.";
         return (
-            <div>
+            <div className={styles.homeContainer}>
                 <div className={styles.titleContainer}>
                     <Title text={title} />
                 </div>
@@ -19,6 +20,9 @@ export default class Home extends Component {
                 </div>
                 <div className={styles.description}>
                     <TextView text={description} />
+                </div>
+                <div className={styles.button}>
+                    <Button text="Learn More" />
                 </div>
             </div>
         )
