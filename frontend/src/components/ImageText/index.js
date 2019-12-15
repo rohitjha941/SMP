c767fb9a8b70c7e2acf20f32a7876c3d5c7ce010
Harshit Maurya <hmaurya999@gmail.com>
2019-12-15 17:39:51 +0530

    create component to display text with image

---
+++ frontend/src/components/ImageText/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CompositeTextView from 'components/CompositeTextView';
+
+import styles from './ImageText.module.scss';
+
+function WrappedComponent(props) {
+    const imageSource = props.imgSrc;
+    const imageAlternativeText = (props.imgAlt) ? props.imgAlt : props.heading;
+    const heading = props.heading;
+    const text = props.text;
+    return (
+        <div className={styles.container} >
+            <img src={imageSource} alt={imageAlternativeText} />
+            <CompositeTextView heading={heading} text={text} />
+        </div>
+    )
+}
+
+const ImageText = React.memo(WrappedComponent);
+export default ImageText;
