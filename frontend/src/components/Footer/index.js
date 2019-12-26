56d1ae97c64363874a65b53e5b127b4c0820c4c7
Harshit Maurya <hmaurya999@gmail.com>
2019-12-26 12:49:21 +0530

    include footer components in footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import Acknowledgement from './Acknowledgement';
+import Sitemap from './Sitemap';
+import ScrollToTop from './ScrollToTop';
+import Branding from './Branding';
 
 import styles from './Footer.module.scss';
 
 function WrappedComponent(props) {
     return (
         <div className={styles.container}>
+            <div className={styles.brandingSitemapContainer}>
+                <Branding/>
+                <Sitemap/>
+            </div>
+            <ScrollToTop/>
             <Acknowledgement />
         </div>
     )
