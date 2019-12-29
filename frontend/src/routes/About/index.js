7c2eedb6056653a29356391877bb6d8ad8891676
Apan Jain <apanjain3@gmail.com>
2019-12-29 23:32:50 +0530

    about us desktop view

--- frontend/src/routes/About/index.js
+++ frontend/src/routes/About/index.js
@@ -4,13 +4,16 @@ import Intro from './Intro';
 import Stats from './Stats';
 import Info from './Info';
 import Team from './Team';
+import styles from './About.module.scss';
 
 export default class About extends Component {
     render() {
         return (
             <div className="about">
-                <Intro />
-                <Stats />
+                <div className={styles.head}>
+                    <Intro />
+                    <Stats />
+                </div>
                 <Info />
                 <Team />
             </div>
