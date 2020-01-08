c6eeb86574be7992506cee5cd28a4c1cb5d6a91e
Harshit Maurya <hmaurya999@gmail.com>
2020-01-08 22:06:12 +0530

    display blog data from api and calculate reading time

--- frontend/src/routes/Home/BlogSection.js
+++ frontend/src/routes/Home/BlogSection.js
@@ -4,21 +4,12 @@ import Button from '../../components/Button';
 import styles from './BlogSection.module.scss';
 import Slider from './Slider';
 
-let blogData1 = {
-    blog_id : 'blog_id',
-    imgSrc : 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
-    imgAlt : 'students',
-    heading : 'Having coffee with faculties at IIT Roorkee', 
-    text : 'Every fresher joining the institute is assigned a mentor (a student of 3/4th year) who they can approach with queries on any issue like academics, extracurricular', 
-    metadata : {d1:'Apan Jain' , d2:'21 Dec\'19', d3:'5 min read'}
-}
-let blogData2 = {
-    blog_id : 'blog_id',
-    imgSrc : 'https://images.unsplash.com/photo-1577201869197-a6df0ba33623?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-    imgAlt : 'students',
-    heading : 'Having coffee with faculties at IIT Roorkee', 
-    text : 'Every fresher joining the institute is assigned a mentor (a student of 3/4th year) who they can approach with queries on any issue like academics, extracurricular', 
-    metadata : {d1:'Apan Jain' , d2:'21 Dec\'19', d3:'5 min read'}
+function calculateReadingTime(text) {
+    const wordsPerMinute = 200;
+    const textLength = text.split(' ').length;
+    const readingTime = Math.ceil(textLength / wordsPerMinute);
+    const result = `~${readingTime} min read`;
+    return result;
 }
 class BlogSection extends Component {
     constructor(){
@@ -33,12 +24,26 @@ class BlogSection extends Component {
             containerType : type
         })
     }
-    render() { 
+    render() {
+        const blogData = this.props.blogs.map(value => {
+            return {
+                blog_id: value.id,
+                imgSrc: value.thumbnail,
+                imgAlt: value.title,
+                heading: value.title,
+                text: value.content,
+                metadata: {
+                    d1: value.author,
+                    d2: value.created_at,
+                    d3: calculateReadingTime(value.content),
+                }
+            }
+        })
         return (  
             <React.Fragment>
                 <div className={styles.cardSection}>
                     <div className={styles.sectionHeading} >Featured Blogs</div>
-                    <Slider blogData={[blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,blogData1,blogData2,]} />
+                    <Slider blogData={blogData} />
                     <Button text='View all Blogs' className={styles.viewBlogsButton} type={'outline'}/>
                 </div>  
             </React.Fragment>
