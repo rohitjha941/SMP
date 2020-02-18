7a9838006293d8d898c32ca7accd91c02a432a84
Apan Jain <apanjain3@gmail.com>
2020-02-19 01:39:55 +0530

    events deskotpView

--- frontend/src/routes/Events/DesktopView.js
+++ frontend/src/routes/Events/DesktopView.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import ComingSoon from '../../components/ComingSoon';
-// import styles from './DesktopView.module.scss';
+// import ComingSoon from '../../components/ComingSoon';
+import styles from './DesktopView.module.scss';
+import EventCard from '../../components/EventCard';
 
 class DesktopView extends Component {
     state = {  }
-    render() { 
+    render() {
+        const eventData = this.props.eventData; 
         return ( 
-            <React.Fragment>
-                <ComingSoon />
-            </React.Fragment>
+            <>
+               <div className={styles.mainHeading}>We Conduct <span className='color-red'>Events</span> year-round</div>
+               <div className={styles.conatiner1}>
+                    <div className={styles.sectionHeading}>Upcoming Events</div>
+                    <ul className={styles.ul1}>
+                        <li><EventCard eventData={eventData} type='side'/></li>
+                        <li><EventCard eventData={eventData} type='side'/></li>
+                    </ul>
+               </div>
+               <div className={styles.container2}>
+                   <div className={styles.sectionHeading}>SMP Events</div>
+                    <ul className={styles.ul2}>
+                        <li><EventCard className={styles.eventcardcommon} eventData={eventData} type='lg'/></li>
+                        <li><EventCard className={styles.eventcardcommon} eventData={eventData} type='lg'/></li>
+                        <li><EventCard className={styles.eventcardcommon} eventData={eventData} type='lg'/></li>
+                        <li><EventCard className={styles.eventcardcommon} eventData={eventData} type='lg'/></li>
+                    </ul>
+               </div>
+            </>
          );
     }
 }
