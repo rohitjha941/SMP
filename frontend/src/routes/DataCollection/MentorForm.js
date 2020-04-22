46481e5724023a7d9e51568b6e0f6513bdad260b
Harshit Maurya <hmaurya999@gmail.com>
2020-04-23 01:31:41 +0530

    namespace bootstrap styles

--- frontend/src/routes/DataCollection/MentorForm.js
+++ frontend/src/routes/DataCollection/MentorForm.js
@@ -4,7 +4,6 @@ import styles from "./MentorForm.module.scss";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
 import CreatableSelect from "react-select/creatable";
-import "bootstrap/dist/css/bootstrap.css";
 import { createMentor } from "api/methods";
 import LoadingOverlay from "../../components/LoadingOverlay";
 import { Redirect } from "react-router-dom";
@@ -258,13 +257,13 @@ class MentorForm extends Component {
             Mentors' <span className="color-red">Data</span>
           </h2>
           <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="name">
                 Name<span className="color-red">*</span>
               </label>
               <input
                 type="text"
-                className="form-control"
+                className={styles["form-control"]}
                 name="name"
                 value={this.state.name}
                 id="name"
@@ -273,13 +272,13 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="enrollno">
                 Enrollment Number<span className="color-red">*</span>
               </label>
               <input
                 type="number"
-                className="form-control"
+                className={styles["form-control"]}
                 name="enrollno"
                 value={this.state.enrollno}
                 id="enrollno"
@@ -290,7 +289,7 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="year">
                 Year<span className="color-red">*</span>
               </label>
@@ -301,7 +300,7 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="branch">
                 Branch<span className="color-red">*</span>
               </label>
@@ -312,7 +311,7 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="groups">
                 Campus Groups that you are/were a part of?
                 <span className="color-red">*</span>
@@ -326,14 +325,14 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="career">
                 Profile in which you are planning your career
                 <span className="color-red">*</span>
               </label>
               <input
                 type="text"
-                className="form-control"
+                className={styles["form-control"]}
                 name="career"
                 value={this.state.career}
                 id="careeer"
@@ -342,7 +341,7 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="interests">
                 Interests or Hobbies in which you invest significant time?
                 <span className="color-red">*</span>
@@ -357,17 +356,17 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="mobile">
                 Mobile Number<span className="color-red">*</span>
               </label>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <div className="input-group-text">+91</div>
+              <div className={styles["input-group"]}>
+                <div className={styles["input-group-prepend"]}>
+                  <div className={styles["input-group-text"]}>+91</div>
                 </div>
                 <input
                   type="number"
-                  className="form-control"
+                  className={styles["form-control"]}
                   name="mobile"
                   value={this.state.mobile}
                   id="mobile"
@@ -378,13 +377,13 @@ class MentorForm extends Component {
                 />
               </div>
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="email">
                 Email address <span className="color-red">*</span>
               </label>
               <input
                 type="email"
-                className="form-control"
+                className={styles["form-control"]}
                 value={this.state.email}
                 id="email"
                 name="email"
@@ -393,13 +392,13 @@ class MentorForm extends Component {
                 required
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="photo">
                 Photograph<span className="color-red">*</span>
               </label>
               <input
                 type="file"
-                className="form-control-file"
+                className={styles["form-control-file"]}
                 name="photo"
                 aria-describedby="photo-help"
                 id="photo"
@@ -407,17 +406,20 @@ class MentorForm extends Component {
                 required
                 onChange={this.handleImage}
               />
-              <small id="photo-help" className="form-text text-muted">
+              <small
+                id="photo-help"
+                className={`${styles["form-text"]} ${styles["text-muted"]}`}
+              >
                 Photograph should be in 1:1 Aspect Ratio
               </small>
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="resume">
                 Resume<span className="color-red">*</span>
               </label>
               <input
                 type="file"
-                className="form-control-file"
+                className={styles["form-control-file"]}
                 name="resume"
                 id="resume"
                 required
@@ -425,7 +427,7 @@ class MentorForm extends Component {
                 accept="application/pdf"
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="achievements">
                 Your Achievements (Awards, Competitions, etc.):
               </label>
@@ -434,12 +436,12 @@ class MentorForm extends Component {
                   <div key={index} id="achievements">
                     <input
                       type="text"
-                      className="form-control"
+                      className={styles["form-control"]}
                       value={achievement}
                       onChange={(e) => this.handleChangeAchievement(e, index)}
                     />
                     <button
-                      className="btn btn-sm btn-outline-danger"
+                      className={`${styles["btn"]} ${styles["btn-sm"]} ${styles["btn-outline-danger"]}`}
                       onClick={(e) => this.handleRemoveAchievement(e, index)}
                     >
                       Remove
@@ -450,28 +452,28 @@ class MentorForm extends Component {
               })}
               <hr />
               <button
-                className="btn btn-sm btn-outline-success"
+                className={`${styles["btn"]} ${styles["btn-sm"]} ${styles["btn-outline-success"]}`}
                 onClick={(e) => this.handleAddAchievement(e)}
               >
                 Add Achievement
               </button>
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="interns">Internships</label>
               {this.state.internships.map((internship, index) => {
                 return (
                   <div key={index} id="interns">
-                    <div className="form-group row">
+                    <div className={`${styles["form-group"]} ${styles.row}`}>
                       <label
                         htmlFor={"company" + index}
-                        className="col-sm-2 col-form-label"
+                        className={`${styles["col-sm-2"]} ${styles["col-form-label"]}`}
                       >
                         Name of Commpany:
                       </label>
-                      <div className="col-sm-10">
+                      <div className={styles["col-sm-10"]}>
                         <input
                           type="text"
-                          className="form-control"
+                          className={styles["form-control"]}
                           value={internship.company}
                           id={"company" + index}
                           onChange={(e) =>
@@ -480,17 +482,17 @@ class MentorForm extends Component {
                         />
                       </div>
                     </div>
-                    <div className="form-group row">
+                    <div className={`${styles["form-group"]} ${styles.row}`}>
                       <label
                         htmlFor={"duration" + index}
-                        className="col-sm-2 col-form-label"
+                        className={`${styles["col-sm-2"]} ${styles["col-form-label"]}`}
                       >
                         Duration of Internship:
                       </label>
-                      <div className="col-sm-10">
+                      <div className={styles["col-sm-10"]}>
                         <input
                           type="text"
-                          className="form-control"
+                          className={styles["form-control"]}
                           value={internship.duration}
                           id={"duration" + index}
                           onChange={(e) =>
@@ -499,17 +501,17 @@ class MentorForm extends Component {
                         />
                       </div>
                     </div>
-                    <div className="form-group row">
+                    <div className={`${styles["form-group"]} ${styles.row}`}>
                       <label
                         htmlFor={"domain" + index}
-                        className="col-sm-2 col-form-label"
+                        className={`${styles["col-sm-2"]} ${styles["col-form-label"]}`}
                       >
                         Domain of Internship:
                       </label>
-                      <div className="col-sm-10">
+                      <div className={styles["col-sm-10"]}>
                         <input
                           type="text"
-                          className="form-control"
+                          className={styles["form-control"]}
                           value={internship.domain}
                           id={"domain" + index}
                           onChange={(e) =>
@@ -519,7 +521,7 @@ class MentorForm extends Component {
                       </div>
                     </div>
                     <button
-                      className="bbtn btn-sm btn-outline-danger"
+                      className={`${styles["btn"]} ${styles["btn-sm"]} ${styles["btn-outline-danger"]}`}
                       onClick={(e) => this.handleRemoveInternship(e, index)}
                     >
                       Remove
@@ -530,17 +532,17 @@ class MentorForm extends Component {
               })}
               <hr />
               <button
-                className="btn btn-sm btn-outline-success"
+                className={`${styles["btn"]} ${styles["btn-sm"]} ${styles["btn-outline-success"]}`}
                 onClick={(e) => this.handleAddInternship(e)}
               >
                 Add Internship
               </button>
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="facebook">Facebook URL</label>
               <input
                 type="url"
-                className="form-control"
+                className={styles["form-control"]}
                 value={this.state.facebook}
                 id="facebook"
                 name="facebook"
@@ -548,11 +550,11 @@ class MentorForm extends Component {
                 onChange={this.handleChange}
               />
             </div>
-            <div className="form-group">
+            <div className={styles["form-group"]}>
               <label htmlFor="linkedin">LinkedIn URL</label>
               <input
                 type="url"
-                className="form-control"
+                className={styles["form-control"]}
                 value={this.state.linkedin}
                 id="linkedin"
                 name="linkedin"
@@ -560,11 +562,7 @@ class MentorForm extends Component {
                 onChange={this.handleChange}
               />
             </div>
-            <Button
-              type="submit"
-              className={"btn btn-primary " + styles.button}
-              text="Submit"
-            />
+            <Button type="submit" className={styles["button"]} text="Submit" />
           </form>
         </div>
       </>
