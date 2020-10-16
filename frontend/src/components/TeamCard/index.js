65875da9913e99dee63af40f7313ece0582b204b
Apan Jain <apanjain3@gmail.com>
2020-10-16 23:11:15 +0530

    add frontend sorting and modify Team Card

--- frontend/src/components/TeamCard/index.js
+++ frontend/src/components/TeamCard/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import styles from "./TeamCard.module.scss";
 import fb from "assets/images/fb.svg";
 import call from "assets/images/call.svg";
-
+import linkedin from "assets/images/linkedin.svg";
 function WrappedComponent(props) {
+  console.log(props.name);
   return (
     <div className={styles.container}>
       <div className={styles.imageparent}>
@@ -22,11 +23,25 @@ function WrappedComponent(props) {
             <img className={styles.commonIcon} src={fb} alt={"facebook"} />
           </div>
         </a>
-        <a href={"tel:" + props.member.contact}>
-          <div className={styles.call}>
-            <img className={styles.commonIcon} src={call} alt={"phone"} />
-          </div>
-        </a>
+        {props.member.linkedin !== undefined &&
+        props.member.linkedin !== null &&
+        props.member.linkedin !== "" ? (
+          <a href={props.member.linkedin}>
+            <div className={styles.fb}>
+              <img
+                className={styles.commonIcon + " " + styles.linkedin}
+                src={linkedin}
+                alt={"linkedin"}
+              />
+            </div>
+          </a>
+        ) : (
+          <a href={"tel:" + props.member.contact}>
+            <div className={styles.call}>
+              <img className={styles.commonIcon} src={call} alt={"phone"} />
+            </div>
+          </a>
+        )}
       </div>
     </div>
   );
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -5,3 +5,59 @@ export function calculateReadingTime(text) {
   const result = `~${readingTime} min read`;
   return result;
 }
+
+export function sortTeam(team) {
+  const firstYear = team
+    .filter((person) => {
+      return person.year === "1st";
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      else return 0;
+    });
+  const secondYear = team
+    .filter((person) => {
+      return person.year === "2nd";
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      else return 0;
+    });
+  const thirdYear = team
+    .filter((person) => {
+      return person.year === "3rd";
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      else return 0;
+    });
+  const fourthYear = team
+    .filter((person) => {
+      return person.year === "4th";
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      else return 0;
+    });
+  const fifthYear = team
+    .filter((person) => {
+      return person.year === "5th";
+    })
+    .sort((a, b) => {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      else return 0;
+    });
+  const sortedTeam = [
+    ...fifthYear,
+    ...fourthYear,
+    ...thirdYear,
+    ...secondYear,
+    ...firstYear,
+  ];
+  return sortedTeam;
+}
