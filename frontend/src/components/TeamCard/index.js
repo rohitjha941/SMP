6cb05a1808eeaf6470403f53d2ef83af0ba429d1
Apan Jain <apanjain3@gmail.com>
2020-10-17 17:33:17 +0530

    update team card for more informative contact info

--- frontend/src/components/TeamCard/index.js
+++ frontend/src/components/TeamCard/index.js
@@ -1,10 +1,44 @@
 import React from "react";
 import styles from "./TeamCard.module.scss";
-import fb from "assets/images/fb.svg";
-import call from "assets/images/call.svg";
-import linkedin from "assets/images/linkedin.svg";
+import fbsrc from "assets/images/fb.svg";
+import callsrc from "assets/images/call.svg";
+import linkedinsrc from "assets/images/linkedin.svg";
 function WrappedComponent(props) {
-  console.log(props.name);
+  const fb = props.member.fb;
+  const linkedin = props.member.linkedin;
+  const contact = props.member.contact;
+  const availability = [
+    {
+      name: "fb",
+      isAvailable: fb !== undefined && fb !== null && fb !== "",
+      link: fb,
+      src: fbsrc,
+    },
+    {
+      name: "linkedin",
+      isAvailable:
+        linkedin !== undefined && linkedin !== null && linkedin !== "",
+      link: linkedin,
+      src: linkedinsrc,
+    },
+    {
+      name: "call",
+      isAvailable: contact !== undefined && contact !== null && contact !== "",
+      link: contact,
+      src: callsrc,
+    },
+  ];
+  let final = availability.filter((item) => {
+    return item.isAvailable;
+  });
+  if (final.length > 2) final.pop();
+  else if (final.length === 1) {
+    if (final[0].name === "fb") final.push(availability[1]);
+    else final.unshift(availability[0]);
+  } else if (final.length === 0) {
+    final.push(availability[0]);
+    final.push(availability[1]);
+  }
   return (
     <div className={styles.container}>
       <div className={styles.imageparent}>
@@ -18,30 +52,20 @@ function WrappedComponent(props) {
       <div className={styles.name}>{props.member.name}</div>
       <div className={styles.designation}>{props.member.designation}</div>
       <div className={styles.contact}>
-        <a href={props.member.fb}>
-          <div className={styles.fb}>
-            <img className={styles.commonIcon} src={fb} alt={"facebook"} />
-          </div>
-        </a>
-        {props.member.linkedin !== undefined &&
-        props.member.linkedin !== null &&
-        props.member.linkedin !== "" ? (
-          <a href={props.member.linkedin}>
-            <div className={styles.fb}>
-              <img
-                className={styles.commonIcon + " " + styles.linkedin}
-                src={linkedin}
-                alt={"linkedin"}
-              />
-            </div>
-          </a>
-        ) : (
-          <a href={"tel:" + props.member.contact}>
-            <div className={styles.call}>
-              <img className={styles.commonIcon} src={call} alt={"phone"} />
-            </div>
-          </a>
-        )}
+        {final.map((item, index) => {
+          return (
+            <span
+              className={!item.isAvailable ? styles.disabled : null}
+              key={index}
+            >
+              <a href={(item.name === "call" ? "tel:" : "") + item.link}>
+                <div className={styles[item.name]}>
+                  <img src={item.src} alt={item.name} />
+                </div>
+              </a>
+            </span>
+          );
+        })}
       </div>
     </div>
   );
